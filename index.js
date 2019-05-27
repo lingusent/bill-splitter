28152e687d9ba45c4e5a3bbcf506378357cfd4b3
lingusent <lingusent@gmail.com>
2019-05-26 19:25:39 -0700

    Created starting state for the app.

---
+++ index.js
@@ -0,0 +1,27 @@
+// Everything else on this line is a comment
+/*
+Everything between the symbol above and the symbol below is a comment.
+*/
+
+/*
+Actions:
+- Add a receipt item
+- Remove a receipt item
+- Add a guest
+- Remove a guest
+- Adding a guest to a receipt item
+- Removing a guest from a receipt item
+- Set a tax rate
+- Set a tip rate
+
+Calculations:
+- Receipt subtotal
+- Receipt tax
+- Receipt tip
+- Receipt total
+- Guest portion of receipt item
+- Guest subtotal
+- Guest tax
+- Guest tip
+- Guest total
+*/
